fff6b77181dfdabac2afa394758d6d4d1463c13f
Ubuntu <ubuntu@ip-172-31-31-242.ec2.internal>
2018-05-30 06:43:55 +0000

    Initial commit

---
+++ reactive/simple_react.py
@@ -0,0 +1,18 @@
+from charms.reactive import when, when_not, set_state
+
+
+@when_not('simple-react.installed')
+def install_simple_react():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    print("I hope this is logged!")
+    set_state('simple-react.installed')
